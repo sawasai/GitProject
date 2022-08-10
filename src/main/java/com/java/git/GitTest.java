897f08bffce61e1ecb2ebee9bd46caf687b6664f
sawa <sawa@java.com>
2022-08-10 18:42:55 +0800

    master third commit

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello hot-fix");
+        System.out.println("Hello master test");
     }
 }
